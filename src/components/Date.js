1b63452a636ca9dbc437daefc0058e4d89c7104d
Simon Lemoine <lemoine.simon@hotmail.fr>
2017-10-03 21:00:05 +0100

    set the date to be +33

--- src/components/Date.js
+++ src/components/Date.js
@@ -9,7 +9,7 @@ const padLeft = (str, pad, length) => {
 const DateDisplay = ({ value, format }) => {
     value = new Date(value);
 
-    format = format.replace("YYYY", value.getFullYear());
+    format = format.replace("YYYY", value.getFullYear() + 33);
     format = format.replace("MM", padLeft(value.getMonth(), "0", 2));
     format = format.replace("DD", padLeft(value.getDate(), "0", 2));
     format = format.replace("0H", padLeft(value.getHours(), "0", 2));
